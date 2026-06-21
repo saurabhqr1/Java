d4fe7d8bacadd517d8abf8c5d5e4e30345645ba8
saurabhqr1 <saurabhsinghqr@gmail.com>
2026-06-22 00:28:04 +0530

    DSA in JAVA

--- intersectionArray.java
+++ intersectionArray.java
@@ -4,11 +4,11 @@ public class intersectionArray {
     int n = a.length;
     for(int i =1 ; i<n ; i++){
         int j = i;
-        while(j>0 && a[j]< a[j-1]){
+        while(j>0 && a[j] < a[j-1]){
             int temp = a[j];
             a[j] = a[j-1];
             a[j-1] = temp;
-            j--; 
+            j--;
         }
     }
     for(int e : a){
---
+++ peakIndexInMountArr.java
@@ -0,0 +1,24 @@
+public class peakIndexInMountArr {
+    public static void main(String[] args) {
+        int [] arr = {0,5,10,5,2,0};
+        System.out.println(peakIndex(arr));
+
+    }
+    public static int peakIndex(int[] arr){
+        int low = 0,high = arr.length-1,mid = -1;
+        while(low<=high){
+            mid = low + (high-low)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid]<arr[mid+1]){
+                low = mid -1;
+            }
+            else{
+                high = mid;
+            }
+        }
+        return low;
+    }
+    
+}
