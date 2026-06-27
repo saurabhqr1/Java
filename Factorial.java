e9297ebc1ce36360bd677edb0994b125edba3c5f
saurabhqr1 <saurabhsinghqr@gmail.com>
2026-06-28 02:26:03 +0530

    DSA SERIES QUESTIONS

--- Factorial.java
+++ Factorial.java
@@ -5,22 +5,17 @@ public class Factorial {
             System.out.println("Factorial is not defined for negetive values.");
             return -1;
         }
-
         if(n==1 || n==0){
             return 1;
         }
         else{
             return (n*Fact(n-1));
         }
-
     }
-
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int value = Fact(a);
         System.out.println(value);
-
     }
-    
 }
